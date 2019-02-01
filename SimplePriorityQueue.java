6dd3002fb640efcef9e2890b7c718d3714bb0ca6
Morgan-Mischo <46653585+Morgan-Mischo@users.noreply.github.com>
2019-01-31 16:53:33 -0800

    Last update
    All I did was add in comments for all of the methods

--- SimplePriorityQueue.java
+++ SimplePriorityQueue.java
@@ -1,62 +1,555 @@
+
 package assign03;
 
+
+
 import java.lang.reflect.Array;
+
 import java.util.ArrayList;
+
 import java.util.Collection;
+
 import java.util.Collections;
+
 import java.util.NoSuchElementException;
+
 import java.util.Comparator;
+
 import java.util.List;
 
 
+
+
+
 /**
+
  * 
+
  * @author Morgan Mischo and Casey Rand
- *
- * @param <E>
+
+ * A class that can perform functions on any collection of inputs, with or without a comparator input
+
  */
+
 public class SimplePriorityQueue<E> implements PriorityQueue {
+
 	
+
 	private E[] arr; 
+
 	private int size;
+
 	private Comparator<? super E> ourComparator; 
+
+	
+
+
+
+	/**
+
+	 * Constructor for SimplePriorityQueue if no comparator is input by user
+
+	 */
+
+	@SuppressWarnings("unchecked")
+
+	public SimplePriorityQueue()
+
+	{
+
+		arr = (E[])new Object[16]; 
+
+		size = 0; 
+
+		ourComparator = null; 
+
+	}
+
 	
 
 	/**
-	 * Constructor for SimplePriorityQueue w/out params
+
+	 * Constructor for SimplePriorityQueue if a comparator is input by user
+
 	 */
-	@SuppressWarnings("unchecked")
-	public SimplePriorityQueue()
-	{
-		arr = (E[])new Object[16]; 
-		size = 0; 
-		ourComparator = null; 
-	}
+
 	
-	/**
-	 * Constructor for SimplePriorityQueue w/ params
-	 */
-	
+
 	@SuppressWarnings("unchecked")
+
 	public SimplePriorityQueue (Comparator<? super E> userComparator)
+
 	{
+
 		arr = (E[])new Object[16]; 
+
 		size = 0; 
+
 		ourComparator = userComparator; 	
+
 	}
+
+
+	/**
+
+	 * Retrieves, but does not remove, the minimum element in this priority queue.
+
+	 * 
+
+	 *  @return the minimum element
+
+	 *  @throws NoSuchElementException if the priority queue is empty 
+
+	 */
+
 
 	@Override
+
 	public Object findMin() throws NoSuchElementException {
+
 		if(size == 0)
+
 		{
+
 			throw new NoSuchElementException("No elements found.");
+
 		}
 
+
+
 		return arr[size - 1]; 
+
 	}
 
+
+	/**
+
+	 * Retrieves and removes the minimum element in this priority queue.
+
+	 * 
+
+	 * @return the minimum element
+
+	 * @throws NoSuchElementException if the priority queue is empty
+
+	 */
+
 	@SuppressWarnings("unchecked")
+
 	@Override
+
+	public Object deleteMin() throws NoSuchElementException {
+
+		if(size == 0)
+
+		{
+
+			throw new NoSuchElementException("No elements found.");
+
+		}
+
+
+
+		Object lastValue = arr[size - 1];
+
+		
+
+		E[] newArray = (E[])new Object[size - 1]; 
+
+		
+
+		for(int i = 0; i < size - 1; i++)
+
+		{
+
+				newArray[i] = arr[i]; 
+
+		}
+
+		
+
+		arr = newArray;
+
+		size--;
+
+		return lastValue;
+
+	}
+
+
+	/** 
+
+	 * Inserts the specified element into this priority queue. 
+
+	 * 
+
+	 * @param item - the element to insert 
+
+	 */
+
+	@SuppressWarnings("unchecked")
+
+	@Override
+
+	public void insert(Object item) {
+
+		int low = 0;
+
+		int high = size;
+
+		
+
+		//Create new larger array to store values and have room for insertion.
+
+		E[] newArray;
+
+		
+
+		//If array is empty, put item in first element of array.
+
+		if(size == 0)
+
+		{
+
+			arr[0] = (E) item;
+
+			size++;
+
+			return;
+
+		}
+
+		
+
+		//If array has one element, compare and insert.
+
+		if(size == 1)
+
+		{
+
+			if(innerCompare(arr[0], (E) item) == 0 || innerCompare(arr[0], (E) item) > 0)
+
+			{
+
+				arr[1] = (E) item;
+
+			}			
+
+			else
+
+			{
+
+				arr[1] = arr[0];
+
+				arr[0] = (E) item;
+
+			}
+
+			size++;
+
+			return;
+
+		}
+
+		
+
+		//If array is not full, length stays the same.
+
+		if(size < arr.length) 
+
+		{
+
+			newArray = (E[])new Object[arr.length];
+
+			for(int i = 0; i < arr.length; i++)
+
+			{
+
+				newArray[i] = arr[i];
+
+			}
+
+		}
+
+		//Array is full, make larger array.
+
+		else
+
+		{
+
+			newArray = (E[])new Object[size + 1];
+
+		}
+
+		
+
+		//If item is greater than all other elements, insert at beginning.
+
+		if(innerCompare((E) item, arr[0]) > 0)
+
+		{
+
+			shiftArray(0, newArray);
+
+			newArray[0] = (E) item;
+
+			arr = newArray;
+
+			size++;
+
+			return;
+
+		}
+
+		
+
+		//If item is less than all other elements, insert at end.
+
+		if(innerCompare((E) item, arr[size - 1]) < 0)
+
+		{
+
+			newArray[size] = (E) item;
+
+			arr = newArray;
+
+			size++;
+
+			return;
+
+		}
+
+		
+
+		//Perform a binary search and insert where o1 is greater and o2 is less than item.
+
+		E o1;
+
+		E o2;
+
+		
+
+		while(low < high)
+
+		{
+
+			int mid = (low + high) / 2;
+
+			o1 = newArray[mid - 1];
+
+			o2 = newArray[mid];
+
+			
+
+			if(innerCompare(o1, (E) item) == 0 || innerCompare(o1, (E) item) > 0 && innerCompare(o2, (E) item) < 0)  
+
+			{
+
+				shiftArray(mid, newArray);
+
+				newArray[mid] = (E) item;
+
+				arr = newArray;
+
+				break;
+
+			}
+
+			
+
+			if(((Comparable<? super E>) o1).compareTo((E) item) > 0)
+
+			{
+
+				low = mid + 1;
+
+			}
+
+			
+
+			else
+
+			{
+
+				high = mid;
+
+			}
+
+		}
+
+		size++;
+
+	}
+
+	
+
+	/**
+
+	 * Makes room for new element to be inserted when the location has been found.
+
+	 * @param count index where element will be inserted
+
+	 * @param newArray array to preserve new values
+
+	 */
+
+	private void shiftArray(int count, E[] newArray)
+
+	{
+
+		for(int i = size; i > count; i--)
+
+		{
+
+			newArray[i] = newArray[i - 1];
+
+		}
+
+	}
+
+	
+
+	/**
+
+	 * This helper method compares two E elements. If user doesn't input comparator, uses .compareTo.
+
+	 * Else uses .compare
+
+	 * @param o1
+
+	 * @param o2
+
+	 * @return 0 if equal, 1 if o1 is greater, -1 if o2 is greater
+
+	 */
+
+	@SuppressWarnings("unchecked")
+
+	private int innerCompare(E o1, E o2)
+
+	{
+
+		if(ourComparator == null)
+
+		{
+
+			return ((Comparable<? super E>) o1).compareTo(o2);
+
+		}
+
+		return ourComparator.compare(o1, o2);
+
+	}
+
+
+
+	/**
+
+	 * Returns E at specified index
+
+	 * @param i
+
+	 * @return
+
+	 */
+
+	public E getIndex(int i)
+
+	{
+
+		return arr[i];
+
+	}
+
+
+	/** 
+
+	 * Inserts the specified elements into this priority queue. 
+
+	 * 
+
+	 *  @param coll - the collection of elements to insert 
+
+	 */
+
+	@Override
+
+	public void insertAll(Collection coll) {
+
+	
+
+		Object[] myArray = new Object[coll.size()]; 
+
+		coll.toArray(myArray); 
+
+		
+
+		for(int i = 0; i < coll.size();  i++)
+
+		{
+
+			insert(myArray[i]);
+
+		}
+
+		
+
+	}
+
+
+	/** 
+
+	 * @return the number of elements in this priority queue
+
+	*/
+
+
+	@Override
+
+	public int size() {
+
+		return size;
+
+	}
+
+
+
+	/**
+
+	 * Returns true if this priority cue contains no elements.
+
+	 */
+
+	@Override
+
+	public boolean isEmpty() {
+
+		return (size == 0);
+
+	}
+
+
+
+	/**
+
+	 * Removes all of the elements from this priority queue. 
+	 * The queue will be empty when this call returns. 
+
+	 */
+
+	@SuppressWarnings("unchecked")
+
+	@Override
+
+	public void clear() {
+
+		arr = (E[])new Object[16];
+
+		size = 0;
+
+	}
+
+}
 	public Object deleteMin() throws NoSuchElementException {
 		if(size == 0)
 		{
